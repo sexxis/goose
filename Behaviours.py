f63a228e99f7539792b107f5bd3e33d6e66beacd
Dylan Tinianov <dylantinianov@gmail.com>
2016-08-07 23:50:50 -0400

    First commit cuz why not

---
+++ Behaviours.py
@@ -0,0 +1,4 @@
+class Behaviours:
+    def __init__(self):
+        self.greet = ["Hello", "Hi Friend", "Yo"]
+        self.get_menu = ["What is on the menu at v1?"]
---
+++ loobot.py
@@ -0,0 +1,3 @@
+class LooBot:
+    def __init__(self):
+        pass
