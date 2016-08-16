1e7d070537f016e8e3559ba08bbb6d71d9627933
Clive Chan <cc@clive.io>
2016-08-16 01:12:59 -0400

    FINALLY WORKS. ugh output buffering in windows python

--- sexxi_bot.py
+++ sexxi_bot.py
@@ -6,6 +6,19 @@ import behaviours
 import random
 import operator
 
+class Unbuffered(object):
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+   def __getattr__(self, attr):
+       return getattr(self.stream, attr)
+
+import sys
+sys.stdout = Unbuffered(sys.stdout)
+
+
 __author__ = "Waterloo SE'XXI"
 
 keywords = behaviours.KeyWords()
