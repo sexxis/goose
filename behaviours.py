4af705c9cdb304bbfd852696941942835b1562a9
Dylan Tinianov <dylantinianov@gmail.com>
2016-08-17 20:11:18 -0400

    Simplified bot methods
    Created one method to check all phrases; instead of a separate method
    for each key phrase type

--- behaviours.py
+++ behaviours.py
@@ -1,4 +1,5 @@
 # Help out by adding to/ improving these lists!!!
+from waterloo_api_data import connections
 
 
 class KeyWords:
@@ -7,7 +8,7 @@ class KeyWords:
         any number of words in the greeting phrase can still be matched.
     """
     def __init__(self):
-        self.GREETING_PHRASES = [
+        self.GREETING = [
             ["hello", "how", "are", "you", "doing"],
             ["hey", "what 's", "up"],
             ["hi", "how 's", "it", "going"],  # Note: words with 's must have a space before due to TextBlob.tags
@@ -18,7 +19,7 @@ class KeyWords:
             ["hey", "how", "goes", "it"]
         ]
 
-        self.FUN_PHRASES = [
+        self.FUN = [
             ["thanks", "mister", "goose"],
             ["who", "do", "you", "miss", "the", "most"]
         ]
@@ -28,11 +29,11 @@ class KeyWords:
             ["what", "is", "your", "name"]
         ]
 
-        self.MENU_PHRASES = [
+        self.MENU = [
             ["what 's", "on", "the", "menu", "at", "v1"]
         ]
 
-        self.WEATHER_PHRASES = [
+        self.WEATHER = [
             ["what 's", "the", "current", "weather", "in", "waterloo"]
         ]
 
@@ -40,36 +41,37 @@ class KeyWords:
 class Responses:
     """ Contains simple responses for the bot"""
     def __init__(self):
-        self.HELP_RESPONSE = "Try asking: " \
-                             "What is the current weather in Waterloo? " \
-                             "What is on the v1 menu?"
+        self.HELP = "Try asking: " \
+                    "What is the current weather in Waterloo? " \
+                    "What is on the v1 menu?"
 
-        self.GREETING_RESPONSES = ["what's up?",
-                                   "hey!",
-                                   "Today is a great day, how are you?"
-                                   ]
+        self.GREETING = ["what's up?",
+                         "hey!",
+                         "Today is a great day, how are you?"
+                         ]
 
-        self.FUN_RESPONSES = ["np",
-                              "harambe ;-;"
-                              ]
+        self.FUN = ["np",
+                    "harambe ;-;"
+                    ]
 
-        self.SELF_RESPONSES = ["I do and feel nothing, I'm a robot :)",
-                               "My favourite thing is being Sexxi",
-                               "My name is Goose"
-                               ]
+        self.ABOUT_SELF = ["I do and feel nothing, I'm a robot :)",
+                           "My favourite thing is being Sexxi",
+                           "My name is Goose"
+                           ]
 
-        self.MENU_RESPONSES = ["The v1 menu contains: "]  # Will need to look up
+        self.MENU = ["The v1 menu contains: "]  # Will need to look up
 
-        self.WEATHER_RESPONSES = ["The current weather in Waterloo is: ",
-                                  " and no rain",
-                                  " and some rain",
-                                  " and heavy rain"
-                                  ]
+        weather = connections.get_temperature()
+        self.WEATHER = ["The current weather in Waterloo is: " + weather[0]]
+        if weather[1]:  # There is rain
+            self.WEATHER[0] += " and some rain"
+        else:
+            self.WEATHER[0] += " and no rain"
 
-        self.UNSURE_RESPONSES = ["Try telling me something else",
-                                 "Sorry, I didn't quite get that",
-                                 "Can you rephrase that?",
-                                 ]
+        self.UNSURE = ["Try telling me something else",
+                       "Sorry, I didn't quite get that",
+                       "Can you rephrase that?"
+                       ]
 
 
 # Don't check these when assessing phrase similarity
--- sexxi_bot.py
+++ sexxi_bot.py
@@ -1,6 +1,5 @@
 from textblob import TextBlob
 from generate_response import format_response, check_pos_tags
-from waterloo_api_data import connections
 from stringscore import liquidmetal
 import behaviours
 import random
@@ -46,76 +45,27 @@ class SexxiBot:
             if words[part] in behaviours.slang_typo_dict.keys():
                 words[part] = behaviours.slang_typo_dict[words[part]]
         self.user_input = ' '.join(words)
+        return False
 
     def help_check(self):
         if self.user_input.lower() == "help":
-            self.response = responses.HELP_RESPONSE
+            self.response = responses.HELP
             return True  # Stop, as we know what category of input we're dealing with
         return False  # User didn't ask for help, move on to greeting_check
 
-    def greeting_check(self):
-        self.user_input = self.user_input.tags  # Do this once; greeting_check must run first
-        self.input_len = len(self.user_input)                     # Find this once too; will be used for scoring
-        for phrase in keywords.GREETING_PHRASES:
-            score = float()
-            for word in self.user_input:
-                for n in phrase:
-                    if word and n not in behaviours.unimportant_words:
-                        score += liquidmetal.score(n, word[0]) / self.input_len
-            if score >= 0.7:    # Could be increased/ decreased through testing to find more optimal value
-                self.response = random.choice(responses.GREETING_RESPONSES)
-                return True
-        return False
-
-    def fun_check(self):
-        # disclaimer: i don't know any NLP so i'm just matching stuff like "thank mr goose"
-        self.user_input = TextBlob(self.user_input.lower())
-        for phrase in keywords.FUN_PHRASES:
-            if phrase == self.user_input.words:
-                n = [i for i, x in enumerate(keywords.FUN_PHRASES) if x == phrase][0]
-                self.response = responses.FUN_RESPONSES[n]
-                return True
-        return False
-
-    def asked_about_self(self):
-        for phrase in keywords.ABOUT_SELF:
-            score = float()
-            for word in self.user_input:
-                for n in phrase:
-                    if word and n not in behaviours.unimportant_words:
-                        score += liquidmetal.score(n, word[0]) / self.input_len
-            if score >= 0.7:
-                self.response = random.choice(responses.SELF_RESPONSES)
-                return True
-        return False
-
-    def menu_check(self):
-        for phrase in keywords.MENU_PHRASES:
-            score = float()
-            for word in self.user_input:
-                for n in phrase:
-                    if word and n not in behaviours.unimportant_words:
-                        score += liquidmetal.score(n, word[0]) / self.input_len
-            if score >= 0.7:
-                self.response = responses.MENU_RESPONSES[0]
-                return True
-        return False
-
-    def weather_check(self):
-        for phrase in keywords.WEATHER_PHRASES:
-            score = float()
-            for word in self.user_input:
-                for n in phrase:
-                    if word and n not in behaviours.unimportant_words:
-                        score += liquidmetal.score(n, word[0]) / self.input_len
-            if score >= 0.7:
-                temp = connections.get_temperature()
-                self.response = responses.WEATHER_RESPONSES[0] + temp[0]
-                if temp[1]:  # There is rain
-                    self.response += responses.WEATHER_RESPONSES[2]
-                else:
-                    self.response += responses.WEATHER_RESPONSES[1]
-                return True
+    def check_phrase_similarity(self):
+        self.user_input = TextBlob(self.user_input.lower()).tags  # Do this once; greeting_check must run first
+        self.input_len = len(self.user_input)  # Find this once too; will be used for scoring
+        for phrase_type in ['GREETING', 'ABOUT_SELF', 'MENU', 'WEATHER', 'FUN']:
+            for phrase in getattr(keywords, phrase_type):
+                score = float()
+                for word in self.user_input:
+                    for n in phrase:
+                        if word and n not in behaviours.unimportant_words:
+                            score += liquidmetal.score(n, word[0]) / self.input_len
+                if score >= 0.7:  # Could be increased/ decreased through testing to find more optimal value
+                    self.response = random.choice(getattr(responses, phrase_type))
+                    return True
         return False
 
     def create_response(self):  # Not really working yet
@@ -135,23 +85,22 @@ def run_bot(user_message, start):
         return bot.response
 
     bot.user_input = raw_input(user_message)
-    bot.fix_typos()
 
-    # fun_check: do fun_check before greeting_check because we are looking for specific things
+    # fix_typos:    Fix any user typos and slang
+    # check_phrase_similarity:  Check user's input similarity to all key phrases
     # create_response:  If all key phrases fail, we gotta actually make a new sentence
-    for method in ['help_check', 'fun_check', 'greeting_check', 'asked_about_self',
-                   'menu_check', 'weather_check', 'create_response']:
+    for method in ['fix_typos', 'check_phrase_similarity', 'create_response']:
         if operator.methodcaller(method)(bot):
             return bot.response
-    return responses.UNSURE_RESPONSES[0]  # If no response can be created
+    return random.choice(responses.UNSURE)  # If no response can be created
 
 
 # For testing purposes
 def main():
-    start = True
+    print run_bot("Enter a message: ", start=True)
     while 1:
-        print run_bot("Enter a message: ", start)
-        start = False
+        print run_bot("Enter a message: ", start=False)
+
 
 # Make sure it's only when we're running this file directly
 if __name__ == '__main__':
