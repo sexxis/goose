4ce70b9ab886c9e547fa2a3f3ae1663f4f499621
Dylan Tinianov <dylantinianov@gmail.com>
2016-08-22 00:34:52 -0400

    Response Templates

---
+++ goose/response/utils/response_templates.py
@@ -0,0 +1,29 @@
+from jinja2 import Template
+import check_pos_tags  # Will use to find parts
+
+
+class ResponseTemplates(object):
+    def __init__(self, noun, pronoun, verb, adj, prep):
+        self.response = str()
+        self.parts = {'noun': noun, 'verb': verb, 'adj': adj, 'pronoun': pronoun, 'prep': prep}
+
+        # Will have a large amount of templates
+        self.name_is = Template('Hello {{noun}}!')
+        self.like_noun = Template('I like {{noun}}')
+
+    def determine_response_type(self):
+        # Will determine what response template to choose from
+        return 'name_is'  # temporarily return this for testing purposes
+
+    def respond(self, resp_type):
+        # Will add in any found parts to the determined response type
+        return getattr(self, resp_type).render(noun=self.parts['noun'],
+                                               verb=self.parts['verb'],
+                                               adj=self.parts['adj'],
+                                               pronoun=self.parts['pronoun'],
+                                               prep=self.parts['prep'])
+
+# Tests
+if __name__ == '__main__':
+    test = ResponseTemplates(noun="Dylan", pronoun=None, verb=None, adj=None, prep=None)
+    print test.respond('name_is')  # Prints "Hello Dylan!"
